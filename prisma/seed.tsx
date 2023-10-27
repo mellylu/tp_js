5a93749163812ea6330f6c411ffaee102727b598
Melly Lucas <melly.lucas32@yahoo.fr>
2023-10-27 18:50:31 +0200

    requete prisma

--- prisma/seed.tsx
+++ prisma/seed.tsx
@@ -1,7 +1,58 @@
-//connection base de données
-// import { createClient } from "@supabase/supabase-js"
+// exécution des requêtes avec prisma
+// import { PrismaClient } from "@prisma/client"
+const { PrismaClient } = require("@prisma/client")
 
-// const NEXT_PUBLIC_SUPABASE_URL: string = process.env.NEXT_PUBLIC_SUPABASE_URL!
-// const NEXT_PUBLIC_SUPABASE_ANON_KEY: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-// const supabase = createClient(NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY)
-// console.log(supabase)
+const prisma = new PrismaClient()
+
+const postUser = async () => {
+    await prisma.user
+        .create({
+            data: {
+                token: "1d5g2fg21f",
+                email: "mel@yahoo.fr",
+                firstname: "melly",
+                lastname: "lucas",
+                password: "xxxxx",
+            },
+        })
+        .then((data: any) => {
+            console.log(data)
+        })
+        .catch((err: any) => {
+            console.log(err)
+        })
+}
+
+const getIdUser = async () => {
+    await prisma.user
+        .findUnique({
+            where: {
+                id: 1,
+            },
+        })
+        .then((data: any) => {
+            console.log(data)
+        })
+        .catch((err: any) => {
+            console.log(err)
+        })
+}
+
+const getAllUser = async () => {
+    await prisma.user
+        .findMany()
+        .then((data: any) => {
+            console.log(data)
+        })
+        .catch((err: any) => {
+            console.log(err)
+        })
+}
+
+const register = async () => {}
+
+postUser()
+getAllUser()
+
+//prisma generate
+//taper cette ligne de commande pour mettre prisma à jour
