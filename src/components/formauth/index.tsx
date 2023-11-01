8b5c3816e7119ec761fa019e8f242567d9c27069
Melly Lucas <melly.lucas32@yahoo.fr>
2023-11-01 01:54:55 +0100

    styles

--- src/components/formauth/index.tsx
+++ src/components/formauth/index.tsx
@@ -7,50 +7,79 @@ const Index = (props: { title: string; children: any }) => {
     return (
         <div id="divmain">
             {/* <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}> */}
-            <SimpleGrid style={{ flexDirection: "column" }} id="simplegrid" columns={{ md: 2 }}>
-                <Box
-                    mx={"auto"}
+            <div style={{ display: "flex", width: "100%", height: "100%" }}>
+                {/* <SimpleGrid id="simplegrid" columns={{ md: 2 }}> */}
+                <Box id="boxform" style={{ padding: 0, height: "100%" }}>
+                    <div id="form" style={{ width: "100%", height: "100%" }}>
+                        <Image
+                            style={{
+                                // bottom: 0,
+                                // left: 0,
+                                marginRight: 0,
+                                width: "100%",
+                                height: "100%",
+                                // visibility: "hidden",
+                                // position: "absolute",
+
+                                // bottom: 0,
+                                // right: 0,
+
+                                // width: "30%",
+                            }}
+                            alt={"Login Image"}
+                            objectFit={"cover"}
+                            src={Logo}
+                            id="imageform"
+                        />
+                    </div>
+                </Box>
+                <Stack
+                    id="stackform"
+                    width="100%"
+                    height="100%"
+                    // position="absolute"
+                    spacing={8}
+                    // mx={"auto"}
+                    margin="auto"
                     maxW={"lg"}
                     py={12}
                     px={6}
-                    rounded={"lg"}
-                    pr={8}
-                    pl={8}
-                    pt={8}
-                    style={{
-                        margin: "auto",
-                        width: "100%",
-                    }}
                 >
-                    <Stack align={"center"}>
-                        <Heading
-                            pb={8}
-                            style={{ color: "black" }}
-                            fontSize={"4xl"}
-                            textAlign={"center"}
-                        >
-                            {props.title}
-                        </Heading>
-                    </Stack>
-                    {props.children}
-                </Box>
-                {/* </Stack> */}
-
-                <Box id="boxform" style={{ padding: 0, height: "100%" }}>
-                    <Image
+                    <Box
+                        //  rounded={'lg'}
+                        // bg={useColorModeValue("white", "gray.700")}
+                        // boxShadow={"lg"}
+                        // p={8}
+                        mx={"auto"}
+                        maxW={"lg"}
+                        py={12}
+                        px={6}
+                        rounded={"lg"}
+                        pr={8}
+                        pl={8}
+                        pt={8}
                         style={{
-                            // bottom: 0,
-                            // left: 0,
-                            marginRight: 0,
+                            margin: "auto",
                             width: "100%",
-                            height: "100%",
                         }}
-                        alt={"Login Image"}
-                        objectFit={"cover"}
-                        src={Logo}
-                    />
-                </Box>
-            </SimpleGrid>
+                        id="formcontour"
+                    >
+                        <Stack align={"center"}>
+                            <Heading
+                                pb={8}
+                                style={{ color: "black" }}
+                                fontSize={"4xl"}
+                                textAlign={"center"}
+                                id="headingform"
+                            >
+                                {props.title}
+                            </Heading>
+                        </Stack>
+                        {props.children}
+                    </Box>
+                </Stack>
+                {/* </SimpleGrid> */}
+            </div>
         </div>
 
         // <Flex
