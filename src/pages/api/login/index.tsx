20e1ab4d6974679516ea82b43d41f1ba3b1a5478
Melly Lucas <melly.lucas32@yahoo.fr>
2023-11-02 13:45:53 +0100

    dernier commit

--- src/pages/api/login/index.tsx
+++ src/pages/api/login/index.tsx
@@ -9,20 +9,7 @@ const prisma = new PrismaClient()
 
 export default async function POST(req: NextApiRequest, res: NextApiResponse) {
     const { email, password } = req.body
-    const headersGetOnly = {
-        "Access-Control-Allow-Origin": "*",
 
-        "Access-Control-Allow-Methods": "OPTIONS, GET, POST, PUT, DELETE",
-    }
-    // await NextCors(req, res, {
-    //     // Options
-    //     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    //     origin: "*",
-    //     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    // })
-
-    // // Rest of the API logic
-    // res.json({ message: "Hello NextJs Cors!" })
     const user = await prisma.user.findUnique({
         where: { email },
     })
