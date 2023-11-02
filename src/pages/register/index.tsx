256441fd6d33530d62c51c0250e53afe30216c30
iimAtomic <vegbahermerite@gmail.com>
2023-11-02 16:14:38 +0100

    email verificator

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,110 +1,140 @@
-import { Box, HStack, Stack, Text, Link } from "@chakra-ui/react"
-import { useState } from "react"
-import { useRouter } from "next/router"
-import axios from "axios"
-import FormAuth from "../../components/formauth"
-import Input from "../../components/input"
-import Button from "../../components/button"
-import { toast } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-import Head from "next/head"
+import { Box, HStack, Stack, Text, Link } from "@chakra-ui/react";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import FormAuth from "../../components/formauth";
+import Input from "../../components/input";
+import Button from "../../components/button";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Head from "next/head";
 
-export default function Index() {
-    const router = useRouter()
-    const [user, setUser] = useState<Object>({})
-    const handleSubmit = async (e: any) => {
-        e.preventDefault()
-        await axios
-            .post(`http://localhost:3000/api/existEmail`, user)
-            .then(async (data: any) => {
-                if (data.data.auth) {
-                    await axios
-                        .post(`http://localhost:3000/api/register`, user)
-                        .then(res => {
-                            if (res.data.auth) {
-                                toast.success("Inscription enregistrée", {})
-                                router.push("/login")
-                            } else {
-                                toast.error("Erreur inscription", { theme: "dark" })
-                            }
-                        })
-                        .catch(err => {
-                            toast.error(err.response.data.message, {
-                                theme: "dark",
-                            })
-                        })
-                } else {
-                }
-            })
-            .catch(err => {
-                toast.error(err.response.data.message, {
-                    theme: "light",
-                })
-            })
+export default function Register() {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password: string | any[]) => {
+    return password.length >= 8;
+  };
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    if (!validateEmail(user.email)) {
+      toast.error("Format d'email invalide", { theme: "dark" });
+      return;
     }
 
-    return (
-        <FormAuth title="S'INSCRIRE">
-             <Head>
-                <link rel="icon" href="/favic.png" type="image/png" />
-             </Head>
-            <Stack spacing={4}>
-                <HStack>
-                    <Box>
-                        <Input
-                            type="text"
-                            id="firstName"
-                            label="Prénom"
-                            onChange={(e: any) => {
-                                setUser({ ...user, firstname: e.target.value })
-                            }}
-                        />
-                    </Box>
-                    <Box>
-                        <Input
-                            type="text"
-                            id="lastName"
-                            label="Nom"
-                            onChange={(e: any) => {
-                                setUser({ ...user, lastname: e.target.value })
-                            }}
-                        />
-                    </Box>
-                </HStack>
-            </Stack>
+    if (!validatePassword(user.password)) {
+      toast.error("Le mot de passe doit avoir au moins 8 caractères", {
+        theme: "dark",
+      });
+      return;
+    }
+
+    // Continuer avec l'inscription si l'email et le mot de passe sont valides
+    await axios
+      .post(`http://localhost:3000/api/existEmail`, user)
+      .then(async (data) => {
+        if (data.data.auth) {
+          await axios
+            .post(`http://localhost:3000/api/register`, user)
+            .then((res) => {
+              if (res.data.auth) {
+                toast.success("Inscription enregistrée", {});
+                router.push("/login");
+              } else {
+                toast.error("Erreur inscription", { theme: "dark" });
+              }
+            })
+            .catch((err) => {
+              toast.error(err.response.data.message, {
+                theme: "dark",
+              });
+            });
+        } else {
+        }
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message, {
+          theme: "light",
+        });
+      });
+  };
+
+  return (
+    <FormAuth title="S'INSCRIRE">
+      <Head>
+        <link rel="icon" href="/favic.png" type="image/png" />
+      </Head>
+      <Stack spacing={4}>
+        <HStack>
+          <Box>
             <Input
-                type="text"
-                id="email"
-                label="Email"
-                onChange={(e: any) => {
-                    setUser({ ...user, email: e.target.value })
-                }}
-                isRequired
+              type="text"
+              id="firstName"
+              label="Prénom"
+              onChange={(e: { target: { value: any; }; }) => {
+                setUser({ ...user, firstname: e.target.value });
+              }}
             />
+          </Box>
+          <Box>
             <Input
-                id="password"
-                isRequired
-                label="Mot de passe"
-                onChange={(e: any) => {
-                    setUser({ ...user, password: e.target.value })
-                }}
+              type="text"
+              id="lastName"
+              label="Nom"
+              onChange={(e: { target: { value: any; }; }) => {
+                setUser({ ...user, lastname: e.target.value });
+              }}
             />
-            <Stack spacing={10} pt={10}>
-                <Button
-                    title="S'inscrire"
-                    onClick={(e: any) => {
-                        handleSubmit(e)
-                    }}
-                />
-            </Stack>
-            <Stack pt={6}>
-                <Text color={"black"} align={"center"}>
-                    Vous avez déjà un compte ?{" "}
-                    <Link href="/login" style={{ textDecoration: "underline" }} color={"black"}>
-                        Se connecter
-                    </Link>
-                </Text>
-            </Stack>
-        </FormAuth>
-    )
+          </Box>
+        </HStack>
+      </Stack>
+      <Input
+        type="text"
+        id="email"
+        label="Email"
+        onChange={(e: { target: { value: any; }; }) => {
+          setUser({ ...user, email: e.target.value });
+        }}
+        isRequired
+      />
+      <Input
+        id="password"
+        isRequired
+        label="Mot de passe"
+        type="password"
+        onChange={(e: { target: { value: any; }; }) => {
+          setUser({ ...user, password: e.target.value });
+        }}
+      />
+      <Stack spacing={10} pt={10}>
+        <Button
+          title="S'inscrire"
+          onClick={(e: any) => {
+            handleSubmit(e);
+          }}
+        />
+      </Stack>
+      <Stack pt={6}>
+        <Text color={"black"} align={"center"}>
+          Vous avez déjà un compte ?{" "}
+          <Link href="/login" style={{ textDecoration: "underline" }} color={"black"}>
+            Se connecter
+          </Link>
+        </Text>
+      </Stack>
+    </FormAuth>
+  );
 }
