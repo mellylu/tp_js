0589f9aaa7eaf7f2d836d5a9c2634845ebc26cce
Melly Lucas <melly.lucas32@yahoo.fr>
2023-11-03 00:23:27 +0100

    partie corentin

--- src/pages/api/getServices/index.tsx
+++ src/pages/api/getServices/index.tsx
@@ -1,19 +1,19 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next"
+import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 export default async function GET_SERVICES(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const services = await prisma.services.findMany();
+        const services = await prisma.services.findMany()
         res.status(200).send({
-            data: services
-        });
-    } catch (err:any) {
+            data: services,
+        })
+    } catch (err: any) {
         res.status(500).send({
-            message: err.message || "Some error occurred while fetching services."
-        });
+            message: err.message || "Some error occurred while fetching services.",
+        })
     } finally {
-        await prisma.$disconnect();
+        await prisma.$disconnect()
     }
 }
